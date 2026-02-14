27211cc428569e84a2766be27d1a30917e91070c
Marc <marcchime21@gmail.com>
2026-02-14 23:47:32 +0800

    init commit

---
+++ assets/tiledMAP/New Object.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="New Object" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Main_Spritesheet.png" width="512" height="512"/>
+ <tile id="8">
+  <properties>
+   <property name="controller" type="bool" value="true"/>
+   <property name="speed" type="float" value="3"/>
+  </properties>
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="12.0909" y="30.7273">
+    <polygon points="0,0 8.90909,0.0909091 8.86561,-28.6838 0.213439,-28.6838"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
